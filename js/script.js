e6f3913db6125714a151e29babe075b20db8c843
Brian Kim <briankim1512@gmail.com>
2017-07-31 20:57:52 +0900

    Added actual data for the app and refactored the code to accomodate the data appropriately

--- js/script.js
+++ js/script.js
@@ -1,29 +1,57 @@
 //Model
-var sideStatus = true;
 var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.5665, lng: 126.9780},
-        zoom: 7,
-    });
+var tmpLocations = {
+    "locations": [
+        { "address": "51 Belmont St, Boston, MA 02129"},
+        { "address": "21 Tremont St UNIT B, Boston, MA 02129"},
+        { "address": "42 8th St, Boston, MA 02129"},
+        { "address": "10 Lexington Ave, Boston, MA 02129"},
+        { "address": "47 Harvard St APT B101, Boston, MA 02129"}
+    ]
 }
+
 //Presenter
-var viewModel = {
-    changeSideStatus: function() {
-            if (sideStatus==true) {
-                $("aside").css("display", "none");
-                $("#map").css("width", "100%");
-                $("#map").css("margin-left", "0%");
-                google.maps.event.trigger(map, "resize");
-                sideStatus = false;
-            }
-            else {
-                $("aside").css("display", "block");
-                $("#map").css("width", "80%");
-                $("#map").css("margin-left", "20%");
-                google.maps.event.trigger(map, "resize");
-                sideStatus = true;
-            }
+    //Parses location data to allow classes for the entries
+for (i in tmpLocations["locations"]) {
+    tmpLocations["locations"][i]["classID"]=
+        tmpLocations["locations"][i]["address"].replace(/\s|,/g, "");
+}
+function ViewModel() {
+    var self = this;
+    self.locations = tmpLocations["locations"];
+    self.showAside = ko.observable(true);
+    self.filterInput = ko.observable("");
+    //Function to change the visibility of the sidebar
+    self.changeSideStatus = function() {
+        if (self.showAside()==true) {
+            self.showAside(false)
+            $("#map").css("width", "100%");
+            $("#map").css("margin-left", "0%");
+            google.maps.event.trigger(map, "resize");
+        }
+        else {
+            self.showAside(true)
+            $("#map").css("width", "80%");
+            $("#map").css("margin-left", "20%");
+            google.maps.event.trigger(map, "resize");
         }
     };
-ko.applyBindings(viewModel);
+    //Function to highlight the clicked address
+    self.highlight = function(data) {
+        $("."+data["classID"]).css("background-color", "grey");
+        for (i in self.locations) {
+            if (self.locations[i]["classID"]==data["classID"]) {
+                continue;
+            }
+            $("."+self.locations[i]["classID"]).css("background-color", "black");
+        }
+    };
+}
+ko.applyBindings(new ViewModel);
+    //Initialize the map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 32.783, lng: -79.94},
+        zoom: 15,
+    });
+}
