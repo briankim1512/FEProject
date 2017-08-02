5dfeab93439f3062a06c25a643390508de389e09
Brian Kim <briankim1512@gmail.com>
2017-08-02 20:10:05 +0900

    Removed unnecessary files, Added FourSqaure info feature, and Added an additional map marker update feature to the filter function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 //Model
+//Global variables needed to make the app work
 var map;
 var marker;
+var locations;
 var infoWindow;
 var populateInfo;
 var resetFilter;
+//This was made as if the server this application ran on served
+//a JSON file (/js/json/locations.json) after I called a $.getJSON
+//request...
 var tmpLocations = {
     "locations": [
         { "address": "51 Belmont St, Boston, MA 02129"},
@@ -13,61 +18,64 @@ var tmpLocations = {
         { "address": "47 Harvard St APT B101, Boston, MA 02129"}
     ]
 }
-
 //Presenter
-    //Parses location data to allow classes and coordinates for the entries
-for (i in tmpLocations["locations"]) {
-    tmpLocations["locations"][i]["classID"]=
-        tmpLocations["locations"][i]["address"].replace(/\s|,/g, "");
-    (function(i){
-        $.getJSON("https://maps.googleapis.com/maps/api/geocode/json"+
-        "?address="+
-        tmpLocations["locations"][i]["address"].replace(/\s/g, "+")+
-        "&key=AIzaSyBqWKH8t9zkyH1hwW69exWJ_jnLUqtq7Yg", function(data){
-            tmpLocations["locations"][i]["lat"]=
-                data["results"][0]["geometry"]["location"]["lat"];
-            tmpLocations["locations"][i]["lng"]=
-                data["results"][0]["geometry"]["location"]["lng"];
-        });
-    })(i);
-}
-    //Initialize the map
-function InitMap() {
-    var self = this;
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 42.3782, lng: -71.0602},
-        zoom: 15,
-    });
-    //Creates markers for each entry within the database
-    marker = [];
-    infoWindow = new google.maps.InfoWindow();
-    for (i in tmpLocations["locations"]) {
-        marker[i] = new google.maps.Marker({
-            position: {lat: tmpLocations["locations"][i]["lat"],
-                lng: tmpLocations["locations"][i]["lng"]},
-            map: map,
-            title: tmpLocations["locations"][i]["address"],
-            animation: google.maps.Animation.DROP
-        });
-        marker[i].addListener('click', function() {
-            populateInfo(this, infoWindow);
-            resetFilter();
-        });
-    }
-    //Renders an infowindow on the selected address or marker
-    populateInfo = function (marker, infowindow) {
-        if (infowindow.marker != marker) {
-            infowindow.setContent('<div>' + marker.title + '</div>');
-            infowindow.open(map, marker);
-        }
-    }
-}
     //Knockout Presenter initializer
 function ViewModel() {
     var self = this;
-    self.locations = tmpLocations["locations"];
+    self.locations = ko.observable(tmpLocations["locations"]);
+    self.example = ko.observable("will this work?");
     self.showAside = ko.observable(true);
     self.filterInput = ko.observable("");
+    //Parses location data to allow classes and coordinates for the entries
+    //Also appends FourSquare data for more information about the place
+    for (i in self.locations()) {
+        self.locations()[i]["classID"]=
+            self.locations()[i]["address"].replace(/\s|,/g, "");
+        (function(i){
+            $.ajax({
+                url: "https://maps.googleapis.com/maps/api/geocode/json"+
+                "?address="+
+                self.locations()[i]["address"].replace(/\s/g, "+")+
+                "&key=AIzaSyBqWKH8t9zkyH1hwW69exWJ_jnLUqtq7Yg",
+                success: function(data){
+                    self.locations()[i]["lat"]=
+                        data["results"][0]["geometry"]["location"]["lat"];
+                    self.locations()[i]["lng"]=
+                        data["results"][0]["geometry"]["location"]["lng"];
+                },
+                error: function() {
+                    console.log('Could not get coordinates');
+                }
+            });
+            $.ajax({
+                url: 'https://api.foursquare.com/v2/venues/explore?near='+
+                    self.locations()[i]["address"].replace(/\s/g, "+")+
+                    '&v=20170101'+
+                    '&client_id=KW3OM1IGPJMQHKY4UDEK2N54F1EGYABQR1A2CO0KKSDRWGTT'+
+                    '&client_secret=FRKIO5OUJPY3EX4W3Q2SRELXLLK23ZI4QTJZDCPY50VXSC4O',
+                success: function(data) {
+                    self.locations()[i]['fSquare'] += "<h3>Nearby place info " +
+                        "made possible by FourSquare</h3>";
+                    var rSquare = data["response"]["groups"][0]['items'];
+                    for(j in rSquare) {
+                        self.locations()[i]['fSquare'] += '<div><h4>' +
+                            rSquare[j]['venue']['name'] + '</h4>' +
+                            rSquare[j]['tips'][0]['text'] + '<br>' +
+                            '</div><br>';
+                        if(j>2) {
+                            break;
+                        }
+                    }
+                    self.locations()[i]['fSquare']=
+                        self.locations()[i]['fSquare'].replace('undefined', '');
+                },
+                error: function() {
+                    self.locations()[i]['fSquare'] = 'Could not load ' +
+                        'FourSquare data...';
+                }
+            });
+        })(i);
+    }
     //Function to change the visibility of the sidebar
     self.changeSideStatus = function() {
         if (self.showAside()==true) {
@@ -86,35 +94,92 @@ function ViewModel() {
     //Function to highlight the clicked address
     self.highlight = function(data) {
         $("."+data["classID"]).css("background-color", "grey");
-        for (i in self.locations) {
-            if (self.locations[i]["classID"]==data["classID"]) {
+        for (i in self.locations()) {
+            if (self.locations()[i]["classID"]==data["classID"]) {
                 for (j in marker) {
-                    if (self.locations[i]["address"]==marker[j].title) {
+                    if (self.locations()[i]["address"]==marker[j].title) {
                         populateInfo(marker[j], infoWindow);
+                        break;
                     }
                 }
                 continue;
             }
-            $("."+self.locations[i]["classID"]).css("background-color", "black");
+            $("."+self.locations()[i]["classID"]).css("background-color", "black");
         }
     };
     //Function to filter out visible addresses
     self.filterAddresses = function() {
-        for (i in self.locations) {
-            if (self.locations[i]["address"].search(self.filterInput())==-1) {
-                $("."+self.locations[i]["classID"]).css("display", "none");
+        for (i in self.locations()) {
+            if (self.locations()[i]["address"].search(self.filterInput())==-1) {
+                $("."+self.locations()[i]["classID"]).css("display", "none");
             }
             else {
-                $("."+self.locations[i]["classID"]).css("display", "block");
+                $("."+self.locations()[i]["classID"]).css("display", "block");
             }
         }
-        self.filterInput()
+        for (i in marker) {
+            if (marker[i].title.search(self.filterInput())==-1) {
+                marker[i].setMap(null);
+            }
+            else {
+                marker[i].setMap(map);
+            }
+        }
     };
     resetFilter = function() {
-        for (i in self.locations) {
-            $("."+self.locations[i]["classID"]).css("display", "block");
-            $("."+self.locations[i]["classID"]).css("background-color", "black");
+        for (i in self.locations()) {
+            $("."+self.locations()[i]["classID"]).css("display", "block");
+            $("."+self.locations()[i]["classID"]).css("background-color", "black");
+        }
+        for (i in marker) {
+            marker[i].setMap(map);
+        }
+        infoWindow.close();
+        map.setCenter({lat: 42.3782, lng: -71.0602});
+        self.filterInput("");
+    }
+}
+var myViewModel = new ViewModel;
+ko.applyBindings(myViewModel);
+//Initialize the map
+function InitMap() {
+    var self = this;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 42.3782, lng: -71.0602},
+        zoom: 15,
+    });
+    //Creates markers for each entry within the database
+    marker = [];
+    infoWindow = new google.maps.InfoWindow();
+    for (i in tmpLocations["locations"]) {
+        marker[i] = new google.maps.Marker({
+            position: {lat: tmpLocations["locations"][i]["lat"],
+                lng: tmpLocations["locations"][i]["lng"]},
+            map: map,
+            title: tmpLocations["locations"][i]["address"],
+            animation: google.maps.Animation.DROP,
+        });
+        marker[i].addListener('click', function() {
+            resetFilter();
+            populateInfo(this, infoWindow);
+        });
+    }
+    //Renders an infowindow on the selected address or marker
+    populateInfo = function (marker, infowindow) {
+        if (infowindow.marker != marker) {
+            for(i in myViewModel.locations()) {
+                if(myViewModel.locations()[i]['address']==marker.title) {
+                    iSquare = myViewModel.locations()[i]['fSquare'];
+                    if (iSquare==undefined) {
+                        iSquare = "<br>Please wait a moment and try again";
+                    }
+                }
+            }
+            infowindow.setContent('<div><h2>' + marker.title + '</h2></div>'+
+                                  '<div>' + iSquare +
+                                  '</div>');
+            infowindow.open(map, marker);
+            map.setCenter(marker.getPosition());
         }
     }
 }
-ko.applyBindings(new ViewModel);
